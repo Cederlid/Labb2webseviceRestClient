5e9f6c1e491c9a8ef1d2ae3282cebe23d7ed4173
Wijdan Cederlid <wijdan.cederlid@iths.se>
2023-05-05 23:04:50 +0200

    Deleted getters and setters that is not needed

--- src/main/java/com/example/labb2websevicerestclient/Product.java
+++ src/main/java/com/example/labb2websevicerestclient/Product.java
@@ -18,36 +18,8 @@ public class Product {
             this.price = price;
         }
 
-        public void setId(Long id) {
-            this.id = id;
-        }
-
-        public Long getId() {
-            return id;
-        }
-
         public String getName() {
             return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public double getPrice() {
-            return price;
-        }
-
-        public void setPrice(double price) {
-            this.price = price;
-        }
-
-        public Category getCategory() {
-            return category;
-        }
-
-        public void setCategory(Category category) {
-            this.category = category;
         }
 
         @Override
